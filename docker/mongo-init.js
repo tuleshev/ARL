85682e24f89219faaa42973a168e2103cb1ec40b
tuleshev <110178411+tuleshev@users.noreply.github.com>
2022-07-28 19:31:52 +0600

    Update mongo-init.js

--- docker/mongo-init.js
+++ docker/mongo-init.js
@@ -1,2 +1,2 @@
 db.user.drop()
-db.user.insert({ username: 'admin',  password: hex_md5('arlsalt!@#'+'arlpass') })
+db.user.insert({ username: '@dmin123',  password: hex_md5('arlsalt!@#'+'!QAZ2wsx#EDC-pl,') })
